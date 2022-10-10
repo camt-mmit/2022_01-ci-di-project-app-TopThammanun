b200d0ae4bd2a93b7a4c93fc3c45bb13a9201b59
TopThammanun <55555topkung@gmail.com>
2022-10-10 11:07:56 +0700

    test merge

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Go <code>Workflow</code> Test Run Na Ja
+          Go <code>Workflow</code> Test Run merge-to master
         </p>
         <a
           className="App-link"
